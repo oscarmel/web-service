cf28e97ea42f84ea8ca2b6718854500d75413491
Oscarmel <oscarmeldomingo@gmail.com>
2021-08-10 21:34:15 -0700

    changes title

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-<title>Bond Web Service Demo</title>
+<title>Music Albums</title>
 <style>
 	body {font-family:georgia;}
 	.film{
@@ -86,7 +86,7 @@ function bondTemplate(film){
 </script>
 </head>
 	<body>
-	<h1>Bond Web Service!</h1>
+	<h1>Album Web Service!</h1>
 		<a href="year" class="category">Bond Films By Year</a><br />
 		<a href="box" class="category">Bond Films By International Box Office Totals</a>
 		<h3 id="filmtitle">Title Will Go Here</h3>
