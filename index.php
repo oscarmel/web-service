b290ba7ed4d74f22aeeda524242357018e5096d8
Oscarmel <oscarmeldomingo@gmail.com>
2021-08-10 20:30:52 -0700

    load initial files

--- index.php
+++ index.php
@@ -43,7 +43,13 @@ function bondJSON(data){
 //JSON processing data goes here
 	console.log(data);
 
-	$("#output").text(JSON.stingify(data));
+	let myData = JSON.stringify(data,null,4);
+
+	myData = '<pre>' + myData + '</pre>';
+
+	$("#output").html(myData);
+	
+	//$("#output").text(JSON.stingify(data));
 }
 
 </script>
