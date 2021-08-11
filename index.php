803b06ee81427ea3da8e321be4e13b4b6b1c1081
Oscarmel <oscarmeldomingo@gmail.com>
2021-08-10 20:55:20 -0700

    updates page

--- index.php
+++ index.php
@@ -2,7 +2,23 @@
 <head>
 <title>Bond Web Service Demo</title>
 <style>
-body {font-family:georgia;}
+	body {font-family:georgia;}
+
+	.film{
+		border:1px solid #E77DC2;
+		border-radius: 5px;
+		padding: 5px;
+		margin-bottom:5px;
+		position:relative;	
+}
+
+	.pic{
+		position:absolute;
+		right:10px;
+		top:10px;
+	}
+
+
 </style>
 <script src="http://code.jquery.com/jquery-latest.js" type="text/javascript"></script>
 
@@ -48,7 +64,7 @@ function bondJSON(data){
 	myData = '<pre>' + myData + '</pre>';
 
 	$("#output").html(myData);
-	
+
 	//$("#output").text(JSON.stingify(data));
 }
 
@@ -60,7 +76,19 @@ function bondJSON(data){
 		<a href="box" class="category">Bond Films By International Box Office Totals</a>
 		<h3 id="filmtitle">Title Will Go Here</h3>
 		<div id="films">
-			<p>Films will go here</p>
+			<div class="film">
+			<b>"Film":</b>1 <br />
+			<b>"Title":</b>"Dr. No"<br />
+			<b>"Year":</b>1962<br />
+			<b>"Director":</br>"Terence Young"<br />
+			<b>"Producers":</b>"Harry Saltzman and Albert R. Broccoli"<br />
+			<b>"Writers":</b>"Richard Maibaum, Johanna Harwood and Berkely Mather"<br />
+			<b>"Composer":</b>"Monty Norman"<br />
+			<b>"Bond":</b>"Sean Connery"<br />
+			<b>"Budget":</b>"$1,000,000.00"<br />
+			<b>"BoxOffice":</b>"$59,567,035.00"<br />
+			<div class="pic"><img src="thumbnails/dr-no.jpg"/></div>
+			</div>
 		</div>
 		<div id="output">Results go here</div>
 	</body>
